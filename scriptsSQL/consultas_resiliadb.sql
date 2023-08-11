7db4438a1c29bf092c5d2a13b9d7ec7d398efa4b
Joa1zin <jvmm676@gmail.com>
2023-08-10 21:24:00 -0300

    feat: criado o arquivo para a realização das consultas no banco de dados ResiliaDB

---
+++ scriptsSQL/consultas_resiliadb.sql
@@ -0,0 +1,4 @@
+#Selecionar a quantidade total de estudantes cadastrados no banco;
+#Selecionar quais pessoas facilitadoras atuam em mais de uma turma;
+#Criar uma view que selecione a porcentagem de estudantes com status de evasão agrupados por turma;
+#Criar um trigger para ser disparado quando o atributo status de um estudante for atualizado e inserir um novo dado em uma tabela de log.
