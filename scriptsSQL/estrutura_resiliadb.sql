16a5354f7787cf4db29c976d849ab7a51d0710f0
isabellalcvicente <isabellavicente@ufpr.br>
2023-08-13 15:50:26 -0300

    organizando a ordem de precedência das tabelas

--- scriptsSQL/estrutura_resiliadb.sql
+++ scriptsSQL/estrutura_resiliadb.sql
@@ -14,58 +14,6 @@ CREATE TABLE `Enderecos` (
   `pais` varchar(50) NOT NULL
 );
 
-CREATE TABLE `Estudantes` (
-  `id_estudante` int UNIQUE PRIMARY KEY NOT NULL,
-  `nome` varchar(255),
-  `nascimento` date,
-  `cpf` varchar(11),
-  `email` varchar(100),
-  `telefone` varchar(15),
-  `pcd` boolean NOT NULL,
-  `status` varchar(25),
-  `id_turma_fk` int NOT NULL,
-  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_endereco_fk` int NOT NULL,
-  FOREIGN KEY (id_endereco) REFERENCES enderecos(id_endereco) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_financas_aluno` int NOT NULL,
-  FOREIGN KEY (id_financas_aluno) REFERENCES financas_aluno(id_financas_aluno) ON UPDATE CASCADE ON DELETE CASCADE,
-);
-
-CREATE TABLE `Financas_aluno` (
-  `id_financas_aluno` int UNIQUE PRIMARY KEY NOT NULL,
-  `valor_total` int,
-  `forma_pagamento` varchar(25),
-  `status_pagamento` varchar(50),
-  `id_curso_fk` int NOT NULL,
-  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE
-
-);
-
-CREATE TABLE `Pagamento_facilitadores` (
-  `id_pagamento_facilitador` int UNIQUE PRIMARY KEY NOT NULL,
-  `valor_pagamento_modulo` int,
-  `qntd_modulos` int,
-  `data_pagamento` date,
-  `id_curso_fk` int NOT NULL,
-  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_pessoas_facilitadoras_fk` int NOT NULL,
-  FOREIGN KEY (id_pessoa_facilitadora) REFERENCES pessoas_facilitadoras(id_pessoa_facilitadora) ON UPDATE CASCADE ON DELETE CASCADE,
-
-);
-
-CREATE TABLE `Estudante_curso` (
-  `id_estudante_curso` int PRIMARY KEY NOT NULL,
-  `data_matricula` date,
-  `evasao` int,
-  `id_estudante_fk` int NOT NULL,
-  FOREIGN KEY (id_estudante) REFERENCES estudantes(id_estudante) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_curso_fk` int NOT NULL,
-  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_turma_fk` int NOT NULL,
-  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE
-
-);
-
 CREATE TABLE `Pessoas_facilitadoras` (
   `id_pessoa_facilitadora` int UNIQUE PRIMARY KEY NOT NULL,
   `nome` varchar(255),
@@ -77,24 +25,6 @@ CREATE TABLE `Pessoas_facilitadoras` (
   `frente` varchar(25),
   `id_endereco_fk` int NOT NULL,
   FOREIGN KEY (id_endereco) REFERENCES enderecos(id_endereco) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE `Facilitadores_turma` (
-  `id_facilitadores_turma` int UNIQUE PRIMARY KEY NOT NULL,
-  `id_pessoa_facilitadora_fk` int NOT NULL,
-  FOREIGN KEY (id_pessoa_facilitadora) REFERENCES pessoas_facilitadoras(id_pessoa_facilitadora) ON UPDATE CASCADE ON DELETE CASCADE,
-  `id_turmas_fk` int NOT NULL,
-  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE `Turmas` (
-  `id_turma` int UNIQUE PRIMARY KEY NOT NULL,
-  `numero` int,
-  `periodo_aulas` varchar(25),
-  `data_inicio` date,
-  `data_termino` date,
-  `id_curso_fk` int NOT NULL,
-  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE `Modulos` (
@@ -115,3 +45,75 @@ CREATE TABLE `Cursos` (
   `id_modulos_fk` int NOT NULL,
   FOREIGN KEY (id_modulos) REFERENCES modulos(id_modulos) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE `Turmas` (
+  `id_turma` int UNIQUE PRIMARY KEY NOT NULL,
+  `numero` int,
+  `periodo_aulas` varchar(25),
+  `data_inicio` date,
+  `data_termino` date,
+  `id_curso_fk` int NOT NULL,
+  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `Facilitadores_turma` (
+  `id_facilitadores_turma` int UNIQUE PRIMARY KEY NOT NULL,
+  `id_pessoa_facilitadora_fk` int NOT NULL,
+  FOREIGN KEY (id_pessoa_facilitadora) REFERENCES pessoas_facilitadoras(id_pessoa_facilitadora) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_turmas_fk` int NOT NULL,
+  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `Pagamento_facilitadores` (
+  `id_pagamento_facilitador` int UNIQUE PRIMARY KEY NOT NULL,
+  `valor_pagamento_modulo` int,
+  `qntd_modulos` int,
+  `data_pagamento` date,
+  `id_curso_fk` int NOT NULL,
+  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_pessoas_facilitadoras_fk` int NOT NULL,
+  FOREIGN KEY (id_pessoa_facilitadora) REFERENCES pessoas_facilitadoras(id_pessoa_facilitadora) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `Financas_aluno` (
+  `id_financas_aluno` int UNIQUE PRIMARY KEY NOT NULL,
+  `valor_total` int,
+  `forma_pagamento` varchar(25),
+  `status_pagamento` varchar(50),
+  `id_curso_fk` int NOT NULL,
+  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `Estudantes` (
+  `id_estudante` int UNIQUE PRIMARY KEY NOT NULL,
+  `nome` varchar(255),
+  `nascimento` date,
+  `cpf` varchar(11),
+  `email` varchar(100),
+  `telefone` varchar(15),
+  `pcd` boolean NOT NULL,
+  `status` varchar(25),
+  `id_turma_fk` int NOT NULL,
+  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_endereco_fk` int NOT NULL,
+  FOREIGN KEY (id_endereco) REFERENCES enderecos(id_endereco) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_financas_aluno` int NOT NULL,
+  FOREIGN KEY (id_financas_aluno) REFERENCES financas_aluno(id_financas_aluno) ON UPDATE CASCADE ON DELETE CASCADE,
+);
+
+CREATE TABLE `Estudante_curso` (
+  `id_estudante_curso` int PRIMARY KEY NOT NULL,
+  `data_matricula` date,
+  `evasao` int,
+  `id_estudante_fk` int NOT NULL,
+  FOREIGN KEY (id_estudante) REFERENCES estudantes(id_estudante) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_curso_fk` int NOT NULL,
+  FOREIGN KEY (id_curso) REFERENCES cursos(id_curso) ON UPDATE CASCADE ON DELETE CASCADE,
+  `id_turma_fk` int NOT NULL,
+  FOREIGN KEY (id_turma) REFERENCES turmas(id_turma) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+
